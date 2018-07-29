86dcba1adc2c6da655ffbc86211042170073434d
mohit <anand.mohit001@gmail.com>
2018-07-30 00:31:55 +0530

    Mongodb Schema with the datatypes

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/budget');
+
+var Schema = mongoose.Schema;
+
+var Budget_Schema = new Schema({
+		task_name: String,
+		desciption: String,
+		amount: Number,
+		created_time: Date, 
+		updated_time: Date,
+
+
+});
+
 
 var app = express(); 
 
